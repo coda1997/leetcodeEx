777dff49dd30e60543cae882a59f9a043ddec6bc
coda1997 <overlooker1997@gmail.com>
2018-01-05 19:59:37 +0800

    AC ex10

--- src/ex08.kt
+++ src/ex08.kt
@@ -1,43 +1,53 @@
+// the shitty problem, it's sick
 fun myAtoi(str: String): Int {
-    if (str.isEmpty())
-        return 0
     var res = 0
-    var count = 0
-    var falg = false
-    var hasnum = false
-    for (i in str) {
-        if (i == ' ') {
-            if (hasnum) break
-            else continue
+    var ptimes = false
+    var plusTimes = 0
+    var flag = 1
+    loop@ for (c in str) {
+        when (c) {
+            ' ' ->{
+                if(!ptimes&&plusTimes==0){
+                    continue@loop
+                }else{
+                    break@loop
+                }
+            }
+            '+'->{
+               if (plusTimes++==0){
+                   continue@loop
+               } else{
+                   return 0
+               }
+            }
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' -> {
+                if(res<Int.MAX_VALUE&&(res*10+c.toInt()-48<0)){
+                    if (flag==1){
+                        return Int.MAX_VALUE
+                    }else{
+                        return Int.MIN_VALUE
+                    }
+                }
+                ptimes=true
+                res *= 10
+                res += c.toInt()-48
+            }
+            '-' -> {
+                if (plusTimes++==0){
+                    flag=-1
+                }else{
+                    return 0
+                }
+            }
+            else -> return res*flag
 
         }
-        if (i == '+' || i == '-') {
-            if (i == '-') falg = true
-            count++
-            continue
-        }
-        if (!i.isDigit())
-            break
-        res *= 10
-        res += when (i) {
-            '0' -> 0
-            '1' -> 1
-            '2' -> 2
-            '3' -> 3
-            '4' -> 4
-            '5' -> 5
-            '6' -> 6
-            '7' -> 7
-            '8' -> 8
-            '9' -> 9
-            else -> 0
-        }
-        hasnum = true
     }
-    if (count > 1) return 0
-    return if (falg) -res else res
+
+    return res*flag
 }
 
 fun main(args: Array<String>) {
-    println(myAtoi("    010"))
+    println('1'.toInt()-48)
+    println(myAtoi(" - 321"))
 }
