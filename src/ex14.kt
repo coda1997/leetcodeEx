0788452f4c4166938fb521c36e6b509672b92cae
coda1997 <overlooker1997@gmail.com>
2018-01-06 10:03:50 +0800

    AC ex14

---
+++ src/ex14.kt
@@ -0,0 +1,22 @@
+fun longestCommonPrefix(strs: Array<String>): String {
+    if (strs.isEmpty()){
+        return ""
+    }
+    var prefix = strs[0]
+    for (str in strs) {
+        for ((index,value) in str.withIndex()){
+            if (index >= prefix.length || value != prefix[index]) {
+                prefix=prefix.substring(0,index)
+                break
+            }
+        }
+        if (prefix.length>str.length){
+            prefix=prefix.substring(0,str.length)
+        }
+    }
+    return prefix
+}
+
+fun main(args: Array<String>) {
+    println(longestCommonPrefix(arrayOf("a","b")))
+}
