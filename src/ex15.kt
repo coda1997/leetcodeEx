ee2b1fa973b59f9d18f62dc3b268426777594d61
coda1997 <overlooker1997@gmail.com>
2018-01-18 16:12:47 +0800

    AC ex16

--- src/ex15.kt
+++ src/ex15.kt
@@ -1,24 +1,35 @@
 private fun threeSum(nums: IntArray): List<List<Int>> {
-    val table = HashMap<Int,Int>()
-    val list = mutableListOf<List<Int>>()
-    var useless = 0
-    for((index,num) in nums.withIndex()){
-        for(i in index until nums.size){
-            if(table.containsKey((-num-nums[i]))){
-//                list.add(listOf(num,nums[i],-num-nums[i]))
+    val res = mutableListOf<List<Int>>()
+    nums.sort()
 
-                list.add(listOf(1,2,3))
+    var l = 0
+    val boundary = nums.size - 2
+    while (l < boundary) {
+        val target = -nums[l]
+        var i = l+1
+        var r = nums.size-1
+        while (i<r){
+            if (nums[i]+nums[r]>target){
+                r--
+            }else if (nums[i]+nums[r]<target){
+                i++
             }else{
-                table.put(num,index)
+                res.add(listOf(nums[i],nums[l],nums[r]))
+                do{
+                    i++
+                }while (nums[i]==nums[i-1]&&i<r)
             }
         }
 
+        do{
+            l++
+        }while (nums[l]==nums[l-1]&&l<boundary)
     }
-    return null!!
+    return res
 }
 
 fun main(args: Array<String>) {
-    for(i in 1 until  4){
-        println(i)
+    threeSum(intArrayOf(-1,0,1,2,-1,-4)).forEach{
+        println(it)
     }
 }
---
+++ src/ex25.kt
@@ -0,0 +1,35 @@
+fun reverseKGroup(head: ListNode?, k: Int): ListNode? {
+    if(head==null||k==1){
+        return head
+    }
+
+    val fake = ListNode(0)
+    fake.next=head
+    var pre = fake
+    var p:ListNode? = head
+    var i  = 0
+    while (p!=null){
+        i++
+        if (i%k==0){
+            pre = reverse(pre,p.next?:return fake.next)
+            p = pre.next
+        }else{
+            p = p.next
+        }
+    }
+    return fake.next
+}
+
+private fun reverse(pre:ListNode,next:ListNode): ListNode {
+    val last = pre.next
+    var curr = last?.next
+    while (curr!=last){
+        last?.next=curr?.next
+        curr?.next=pre.next
+        pre.next=curr
+        curr=last?.next
+    }
+    return last!!
+
+}
+
