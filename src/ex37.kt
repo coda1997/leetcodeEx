f44898f732cc124807a6cd3e82d941e07c3f68bb
coda1997 <overlooker1997@gmail.com>
2018-01-27 10:01:45 +0800

    AC ex38

---
+++ src/ex37.kt
@@ -0,0 +1,7 @@
+fun solveSudoku(board: Array<CharArray>): Unit {
+TODO()
+}
+
+private fun checkSudokuIsValid(board: Array<CharArray>):Boolean{
+TODO()
+}
---
+++ src/ex38.kt
@@ -0,0 +1,14 @@
+fun countAndSay(n: Int): String {
+    var res = "1"
+    val regax = "1+|2+|3+|4+|5+|6+|7+|8+|9+".toRegex()
+    (1 until n).forEach {
+        val o=StringBuilder()
+        regax.findAll(res).forEach { o.append(it.value.length).append(it.value[0]) }
+        res=o.toString()
+    }
+    return res
+}
+
+fun main(args: Array<String>) {
+    countAndSay(5).apply(::println)
+}
