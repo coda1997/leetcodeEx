b91b37683e9757573710421e69062c8435ad0441
coda1997 <overlooker1997@gmail.com>
2019-12-02 15:53:18 +0800

    solve ex 188

--- src/leetCode/ex188.kt
+++ src/leetCode/ex188.kt
@@ -4,17 +4,23 @@ import kotlin.math.*
 import kotlin.test.*
 
 fun maxProfit(k: Int, prices: IntArray): Int {
-    val cost = Array(k + 1) { IntArray(2) { 0 } }
+    if (prices.isEmpty()) {
+        return 0
+    }
+    val t =
+            if (k > prices.size / 2) {
+                prices.size / 2
+            } else {
+                k
+            }
+    val cost = Array(t + 1) { intArrayOf(0, -prices[0]) }
     for (i in prices.indices) {
-        for (j in 1..k) {
-            cost[j][0] = max(cost[j][0], cost[j - 1][1] + prices[i])
-            cost[j][1] = max(cost[j - 1][1], cost[j - 1][0] - prices[i])
+        for (j in 1..t) {
+            cost[j][0] = max(cost[j][0], cost[j][1] + prices[i])
+            cost[j][1] = max(cost[j][1], cost[j - 1][0] - prices[i])
         }
     }
-    return cost.maxBy { it[0] }!![0]
+    return cost.maxBy { it[0] }?.get(0) ?: 0
 }
 
-fun main() {
-    assertEquals(2, maxProfit(2, intArrayOf(2, 4, 1)))
-    assertEquals(7, maxProfit(2, intArrayOf(3, 2, 6, 5, 0, 3)))
-}
+
