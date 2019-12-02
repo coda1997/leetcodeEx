3d381ce92a3ec4b6731cb30cfcac007f6b94bf39
coda1997 <overlooker1997@gmail.com>
2019-12-02 16:34:59 +0800

    solve ex 189

--- src/leetCode/ex189.kt
+++ src/leetCode/ex189.kt
@@ -1,23 +1,24 @@
 package leetcode
 
 fun rotate(nums: IntArray, k: Int): Unit {
-    val size = nums.size
-    if(size==0){
-        return
-    }
-    val kk = k%size
-    var temp = nums[0]
-    var p = kk
-    var temp2 = nums[p]
-    (0 until size).forEach{
-        nums[p]=temp
-        p = (p+kk)%size
-        temp = temp2
-        temp2 = nums[p]
-    }
-    return
+    val len = nums.size
+    val kk = k%len
+    reverseNums(nums,0,len-1)
+    reverseNums(nums,0,kk-1)
+    reverseNums(nums,kk,len-1)
 }
-
+fun reverseNums(nums: IntArray,start:Int,end:Int){
+    var i = start
+    var j = end
+    var p:Int
+    while (i<j){
+        p = nums[i]
+        nums[i]=nums[j]
+        nums[j]=p
+        i++
+        j--
+    }
+}
 fun main(args: Array<String>) {
     val demo1 = intArrayOf(1,2,3,4,5,6)
     rotate(demo1,3)
