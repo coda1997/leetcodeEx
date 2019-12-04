26a942fed7a2c6da228e699898252e21fedcbdb2
Dadachen <overlooker1997@gmail.com>
2019-12-04 22:54:34 +0800

    solve ex 300

---
+++ src/leetCode/ex300.kt
@@ -0,0 +1,23 @@
+package leetCode
+
+import kotlin.math.max
+import kotlin.test.assertEquals
+
+fun lengthOfLIS(nums: IntArray): Int {
+    if (nums.isEmpty()){
+        return 0
+    }
+    val tt = IntArray(nums.size){1}
+    for (i in nums.indices){
+        for (j in 0 until i){
+            if (nums[i]>nums[j]){
+                tt[i] = max(tt[i],tt[j]+1)
+            }
+        }
+    }
+    return tt.max()?:1
+}
+
+fun main() {
+    assertEquals(4, lengthOfLIS(intArrayOf(10,9,2,5,3,7,101,18)))
+}
