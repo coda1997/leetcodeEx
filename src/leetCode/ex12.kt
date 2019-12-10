8f23efbd28c36e212f3078e812d5b0244a0cd05c
coda1997 <overlooker1997@gmail.com>
2019-12-10 14:32:57 +0800

    do some code beautification

--- src/leetCode/ex12.kt
+++ src/leetCode/ex12.kt
@@ -2,24 +2,24 @@ package leetCode
 
 private fun intToRoman(num: Int): String {
     val s = arrayOf(
-        arrayOf(
-            "","I","II","III","IV","V","VI","VII","VIII","IX"
-        )
-        , arrayOf(
-            "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"
-        )
-        , arrayOf(
-            "","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"
-        )
-        , arrayOf(
-            "","M","MM","MMM"
-        )
+            arrayOf(
+                    "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"
+            )
+            , arrayOf(
+            "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"
+    )
+            , arrayOf(
+            "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"
+    )
+            , arrayOf(
+            "", "M", "MM", "MMM"
+    )
     )
     var roman = ""
-    roman +=(s[3][(num/1000)%10])
-    roman +=(s[2][(num/100)%10])
-    roman +=(s[1][(num/10)%10])
-    roman +=(s[0][num%10])
+    roman += s[3][(num / 1000) % 10]
+    roman += s[2][(num / 100) % 10]
+    roman += s[1][(num / 10) % 10]
+    roman += s[0][num % 10]
     return roman
 }
 
