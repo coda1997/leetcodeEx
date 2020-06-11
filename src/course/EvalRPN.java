179c7f7edc506c4e73310dbf64705945654c4c37
Dadachen <overlooker1997@gmail.com>
2020-06-11 22:25:11 +0800

    update solutions

---
+++ src/course/EvalRPN.java
@@ -0,0 +1,35 @@
+package course;
+
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+public class EvalRPN {
+    public int evalRPN(String[] tokens) {
+        Deque<String> stack = new LinkedList<>();
+        for (var character : tokens) {
+            if(character.equals("+")){
+                int t1 = Integer.parseInt(stack.pop());
+                int t2 = Integer.parseInt(stack.pop());
+                stack.push(t1+t2+"");
+            }else if(character.equals("-")){
+                int t2 = Integer.parseInt(stack.pop());
+                int t1 = Integer.parseInt(stack.pop());
+                stack.push(t1-t2+"");
+
+            }else if(character.equals("*")){
+                int t1 = Integer.parseInt(stack.pop());
+                int t2 = Integer.parseInt(stack.pop());
+                stack.push(t1*t2+"");
+            }else if(character.equals("/")){
+                int t2 = Integer.parseInt(stack.pop());
+                int t1 = Integer.parseInt(stack.pop());
+                stack.push(t1/t2+"");
+            }else{
+                stack.push(character);
+            }
+        }
+        return Integer.parseInt(stack.pop());
+    }
+}
