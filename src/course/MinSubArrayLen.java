257ed06c4cd3f5d5a982e1b61be33b0e939215dc
Dadachen <overlooker1997@gmail.com>
2020-06-24 21:03:01 +0800

    update

--- src/course/MinSubArrayLen.java
+++ src/course/MinSubArrayLen.java
@@ -4,4 +4,8 @@ public class MinSubArrayLen {
     public int minSubArrayLen(int s, int[] nums) {
         return 0;
     }
+
+    public static void main(String[] args) {
+        System.out.println(new MinSubArrayLen().minSubArrayLen(1,new int[]{1}));
+    }
 }
