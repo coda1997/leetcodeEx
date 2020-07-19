d6a0ef2ce06bfbe8c5c30d5a91618460251555e2
DadaChen1997 <overlooker1997@gmail.com>
2020-07-19 15:23:21 +0800

    leetcode 236

---
+++ src/course/LowestCommonAncestor.java
@@ -0,0 +1,38 @@
+package course;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+import leetCode.TreeNode;
+
+public class LowestCommonAncestor {
+
+    
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        if(root==null){
+            return null;
+        }
+        if(root.val==p.val){
+            return root;
+        }
+        if(root.val==q.val){
+            return root;
+        }
+
+        var left = lowestCommonAncestor(root.left, p, q);
+        var right = lowestCommonAncestor(root.right, p, q);
+        if(left!=null&&right!=null){
+            return root;
+        }
+        if(left!=null){
+            return left;
+        }
+        if(right!=null){
+            return right;
+        }
+        return null;
+    }
+
+    
+}
