a89b642e7257502c29c95221c9579f256713205a
dadachen <overlooker1997@gmail.com>
2020-08-19 21:19:50 +0800

    647. 回文子串

--- src/course/CountSubString.java
+++ src/course/CountSubString.java
@@ -1,11 +1,23 @@
 package course;
-
+/**
+ * 给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+ * 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串.
+ */
 public class CountSubString {
     public int countSubstrings(String s) {
         int res = 0;
         var cc = s.toCharArray();
+        var ss = new char[cc.length*2+1];
+        ss[0] = '#';
+        int t = 1;
         for (int i = 0; i < cc.length; i++) {
-            res += helper(cc, i);
+            ss[t]=cc[i]; 
+            t++;
+            ss[t]='#';
+            t++;
+        }
+        for (int i = 0; i < ss.length; i++) {
+            res += helper(ss, i);
         }
         return res;
     }
@@ -19,6 +31,6 @@ public class CountSubString {
             }
             c++;
         }
-        return c;
+        return c/2;
     }
 }
