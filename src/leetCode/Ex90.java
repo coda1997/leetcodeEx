e41df776d8497c7c0635ece1fcced08848b82f0e
DadaChen1997 <overlooker1997@gmail.com>
2020-09-22 14:13:39 +0800

    ex 90

---
+++ src/leetCode/Ex90.java
@@ -0,0 +1,49 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ex90 {
+    private List<List<Integer>> res = new ArrayList<>();
+    private Deque<Integer> temp = new LinkedList<>();
+    private List<int[]> ts = new ArrayList<>();
+    private int len;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if(i==0||nums[i]!=ts.get(ts.size()-1)[0]){
+                ts.add(new int[]{nums[i],1});
+            }else{
+                ts.get(ts.size()-1)[1]++;
+            }
+        }
+        len = ts.size();
+        helper(0);
+        return res;
+    }
+
+    private void helper(int i){
+        if(i==len){
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        helper(i+1);
+        var tt = ts.get(i);
+        var item = tt[0];
+        var c = tt[1];
+        for (int j = 0; j < c; j++) {
+            temp.push(item);
+            helper(i+1);
+        }
+        for (int j = 0; j < c; j++) {
+            temp.pop();
+        }
+    } 
+    public static void main(String[] args) {
+        var o = new Ex90();
+        var t = o.subsetsWithDup(new int[]{1,2,2});
+    }
+}
