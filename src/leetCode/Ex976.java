47a8a621eb0dc7d5a2e623c00b77e01bed0dcffc
coda1997 <overlooker1997@gmail.com>
2020-11-29 10:21:26 +0800

    Ex 976

---
+++ src/leetCode/Ex976.java
@@ -0,0 +1,15 @@
+package leetCode;
+
+import java.util.Arrays;
+
+public class Ex976 {
+    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        for (int i = A.length-1; i >= 2; i--) {
+            if(A[i-2]+A[i-1]>A[i]){
+                return A[i-2]+A[i-1]+A[i];
+            }
+        }
+        return 0;
+    }
+}
