06062fcb802b624d32fa35125bc4929b39a897a8
coda1997 <overlooker1997@gmail.com>
2020-12-20 10:35:01 +0800

    Ex48

--- src/leetCode/Ex316.java
+++ src/leetCode/Ex316.java
@@ -1,10 +1,6 @@
 package leetCode;
 
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
 
 public class Ex316 {
     public String removeDuplicateLetters(String s) {
@@ -33,10 +29,38 @@ public class Ex316 {
         return sb.toString();
     }
 
+    public String removeDuplicateLetters2(String s) {
+        Deque<Character> stack = new LinkedList<>();
+        int[] cs = new int[26];
+        var cc = s.toCharArray();
+        boolean[] visited = new boolean[26];
+        stack.offer(cc[0]);
+        visited[cc[0]-'a'] = true;
+        for (int i = 1; i < cc.length; i++) {
+            cs[cc[i]-'a']++;
+        }
+        for (int i = 1; i < cc.length; i++) {
+            if(!visited[cc[i]-'a']){
+                while (!stack.isEmpty()&&cc[i]<stack.peek()&&cs[stack.peek()-'a']>0){
+                    visited[stack.pop()-'a']=false;
+                }
+                stack.push(cc[i]);
+                visited[cc[i]-'a'] = true;
+            }
+            cs[cc[i]-'a']--;
+        }
+        var sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.insert(0, stack.pop());
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         var o = new Ex316();
-        System.out.println( o.removeDuplicateLetters("bcabc"));
-        System.out.println( o.removeDuplicateLetters("cbacdcbc"));
-        System.out.println( o.removeDuplicateLetters("cdadabcc"));
+        System.out.println( o.removeDuplicateLetters2("bcabc"));
+        System.out.println( o.removeDuplicateLetters2("cbacdcbc"));
+        System.out.println( o.removeDuplicateLetters2("cdadabcc"));
+        System.out.println( o.removeDuplicateLetters2("ecbacba"));
     }
 }
