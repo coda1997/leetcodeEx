6e68937bd76ebe4fc0d5d8aa5447f275dec9a0e1
coda1997 <overlooker1997@gmail.com>
2021-01-07 15:30:47 +0800

    Ex 399 with union-find algorithm

--- src/leetCode/Ex399.java
+++ src/leetCode/Ex399.java
@@ -55,4 +55,80 @@ public class Ex399 {
         }
         return res;
     }
+
+    public double[] calcEquation2(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        int indexs = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < equations.size(); i++) {
+            if (!map.containsKey(equations.get(i).get(0))) {
+                map.put(equations.get(i).get(0), indexs++);
+            }
+            if (!map.containsKey(equations.get(i).get(1))) {
+                map.put(equations.get(i).get(1), indexs++);
+            }
+
+        }
+        FindJoin findJoin = new FindJoin(indexs);
+        for (int i = 0; i < values.length; i++) {
+            int ia = map.get(equations.get(i).get(0));
+            int ib = map.get(equations.get(i).get(1));
+            findJoin.union(ia, ib, values[i]);
+        }
+        double[] res = new double[queries.size()];
+        for (int i = 0; i < res.length; i++) {
+            List<String> item = queries.get(i);
+            int ia = map.getOrDefault(item.get(0), -1);
+            int ib = map.getOrDefault(item.get(1), -1);
+            if (ia == -1 || ib == -1) {
+                res[i] = -1;
+            }else{
+                res[i] = findJoin.findUnion(ia, ib);
+            }
+        }
+
+        return res;
+    }
+
+    class FindJoin{
+        int[] parents;
+        double[] weights;
+        FindJoin(int n){
+            parents = new int[n];
+            for (int i = 0; i < n; i++) {
+                parents[i] = i;
+            }
+            weights = new double[n];
+            Arrays.fill(weights, 1.0);
+        }
+
+
+        int find(int index){
+            if (parents[index] != index) {
+                int origin = parents[index];
+                parents[index] = find(parents[index]);
+                weights[index] *= weights[origin];
+            }
+            return parents[index];
+        }
+        //note it is from i to j, not from j to i
+        void union(int i, int j, double value){
+            int pi = find(i);
+            int pj = find(j);
+            if (pi == pj) {
+                return;
+            }
+            parents[pi] = pj;
+            weights[pi] = value*weights[j]/weights[i];
+        }
+
+        double findUnion(int i, int j){
+            int pi = find(i);
+            int pj = find(j);
+            if (pi != pj) {
+                return -1.0;
+            }else {
+                return weights[i]/weights[j];
+            }
+        }
+    }
 }
