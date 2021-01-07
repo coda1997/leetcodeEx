ca25721fd2146896c9905512538cdd4cddad4c5f
coda1997 <overlooker1997@gmail.com>
2021-01-07 14:32:49 +0800

    Ex 547 with union-find algorithm

--- src/leetCode/Ex547.java
+++ src/leetCode/Ex547.java
@@ -31,9 +31,44 @@ public class Ex547 {
         return res;
     }
 
+    public int findCircleNum2(int[][] isConnected){
+        int len = isConnected.length;
+        int[] parents = new int[len];
+        for (int i = 0; i < len; i++) {
+            parents[i] = i;
+        }
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if(isConnected[i][j]==1){
+                    union(parents, i, j);
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            if(parents[i]==i){
+                res++;
+            }
+        }
+        return res;
+    }
+
+
+    private int find(int[] parent, int index){
+        if (parent[index] != index){
+            parent[index] = find(parent, parent[index]);
+        }
+        return parent[index];
+    }
+
+    private void union(int[] parent, int i, int j){
+        parent[find(parent, i)] = find(parent, j);
+    }
+
+
     public static void main(String[] args) {
         var o = new Ex547();
-        System.out.println(o.findCircleNum(new int[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}));
-        System.out.println(o.findCircleNum(new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
+        System.out.println(o.findCircleNum2(new int[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}));
+        System.out.println(o.findCircleNum2(new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
     }
 }
